ca6b3dc8609e2d82a62162e67f649e0439f490e1
Jordan Brown <jebrown2091@gmail.com>
2022-08-12 11:00:29 +0930

    fixed memory leak

--- 01/ex01/zombieHoard.cpp
+++ 01/ex01/zombieHoard.cpp
@@ -6,7 +6,7 @@
 /*   By: jbrown <jbrown@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 15:08:34 by jbrown            #+#    #+#             */
-/*   Updated: 2022/08/01 13:05:08 by jbrown           ###   ########.fr       */
+/*   Updated: 2022/08/12 10:53:25 by jbrown           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ Zombie* zombieHorde( int N, std::string name )
 	i = 0;
 	while (i < N)
 	{
-		hoard[i] = *newZombie(name);
+		hoard[i].set_name(name);
 		i++;
 	}
 	return (hoard);
